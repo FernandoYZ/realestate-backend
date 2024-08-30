64600ad0b20dbaaff0b9b14543e9632ddc456626
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 06:36:43 -0500

    refactor(dto): actualizar CreateUserDto con validaciones y propiedades
    Se ha modificado el DTO CreateUserDto para incluir validaciones utilizando class-validator. Ahora incluye propiedades como name, lastname, identity_document, gender, email y password, con sus respectivas validaciones para asegurar la integridad de los datos.
    
    Closes #0

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, Length, MinLength } from "class-validator";
+
+export enum Gender {
+    Masculino = 'Masculino',
+    Femenino = 'Femenino',
+    Otros = 'Otros',
+}
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    readonly lastname: string;
+    
+    @IsInt()
+    @IsNotEmpty()
+    @Length(8,8)
+    readonly identity_document: number;
+    
+    @IsOptional()
+    @IsEnum(Gender)
+    readonly gender?: Gender;
+
+    @IsNotEmpty()
+    @IsEmail()
+    readonly email: string;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    readonly password: string;
+}
