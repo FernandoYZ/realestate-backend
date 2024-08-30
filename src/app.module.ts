b759827df45984d56be530569a8ebc18feb09bd4
FernandoYZ <fernandozavala266@gmail.com>
2024-08-29 19:48:52 -0500

    ci[mongodb]: agregó el puerto de mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/realstate'),
+    ProductsModule
+  ],
 })
 export class AppModule {}
