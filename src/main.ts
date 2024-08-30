3e732b9f0af05a1572c7fba160fefde137e52b2d
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 14:27:50 -0500

    feat(exception): agregar filtro global de excepciones
    Se ha añadido el filtro de excepciones global HttpExceptionFilter en main.ts para manejar errores de manera centralizada en toda la aplicación. Esto permite registrar errores y devolver respuestas estructuradas en formato JSON para mejorar la gestión de errores y la experiencia del usuario.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './app.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter);
   app.setGlobalPrefix('api')
   await app.listen(4000);
 }
