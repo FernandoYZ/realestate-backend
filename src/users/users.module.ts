d37b275823d31139b20313f7d00052f1980fcfac
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 06:40:11 -0500

    refactor(module): actualizar UsersModule para incluir Mongoose y JWT
    Se ha modificado el UsersModule para integrar Mongoose y el esquema de usuario, permitiendo la interacción con la base de datos. Además, se ha configurado el módulo JWT para manejar la autenticación de usuarios, estableciendo opciones de firma y expiración para los tokens.
    
    Closes #0

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema, User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: 'jwt_secret',
+      signOptions: { expiresIn: '6h' }, 
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
