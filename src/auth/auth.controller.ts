1978fe0a9787fc01831d1878465c86f0b1fb8f1e
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 07:42:58 -0500

    feat(auth): implementar lógica de autenticación en AuthController
    Se ha actualizado el AuthController para manejar las solicitudes de inicio de sesión y refresco de tokens. Ahora incluye la validación de credenciales de usuario y la gestión de tokens JWT. Se lanzan excepciones adecuadas para credenciales inválidas y para la falta de tokens de refresco, mejorando así la seguridad y la experiencia del usuario.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+        private readonly usersService: UsersService,
+      ) {}
+    
+      @Post('login')
+      async login(@Body() loginDto: { email: string; password: string }) {
+        const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+        if (!user) {
+          throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+        }
+        return await this.authService.login(user);
+      }
+    
+      @Post('refresh')
+      async refreshToken(@Req() request: Request) {
+        const token = request.headers['authorization']?.split(' ')[1];
+        if (!token) {
+          throw new HttpException('Refresh token not provided', HttpStatus.BAD_REQUEST);
+        }
+        return await this.authService.refreshToken(token);
+      }
+}
