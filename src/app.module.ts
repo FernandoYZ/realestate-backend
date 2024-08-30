c228f4f7cdddba8964f296b435ab6b5255c1fffd
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 06:45:44 -0500

    feat(auth): agregar módulo, controlador y servicio de autenticación
    Se ha creado un nuevo módulo de autenticación que incluye el controlador y el servicio necesarios para manejar todas las funcionalidades relacionadas con la autenticación. Esto incluye la gestión de JWT, la validación de usuarios y la protección de rutas, mejorando la modularidad y separación de responsabilidades en la aplicación.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/realstate'),
     ProductsModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
