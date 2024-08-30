d0dca8703401324bfaf4eca36575ee3239290656
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 14:34:20 -0500

    feat(app): agregar middleware de autenticación para rutas protegidas
    Se ha actualizado el AppModule para incluir el AuthMiddleware, que verifica la autenticación de los usuarios en las rutas protegidas. El middleware se aplica a las rutas que comienzan con 'protected', asegurando que solo los usuarios autenticados puedan acceder a estas. Esta mejora refuerza la seguridad de la aplicación al centralizar la lógica de autenticación en un middleware dedicado.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   imports: [
@@ -12,4 +13,10 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('protected');
+  }
+}
