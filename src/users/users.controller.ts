e18fee9a285d4da8fdf623d5bb8451c51e00482d
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 07:45:48 -0500

    refactor(users): eliminar lógica de autenticación del UsersController
    Se ha actualizado el UsersController para eliminar los métodos de inicio de sesión y refresco de tokens, enfocándose únicamente en las operaciones CRUD para la gestión de usuarios. Esto mejora la claridad y la separación de responsabilidades, delegando la autenticación al AuthService y manteniendo el controlador centrado en la gestión de usuarios.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -11,21 +10,6 @@ export class UsersController {
   @Post('register')
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
-  }
-
-  @Post('login')
-  async login(@Body() loginDto: { email: string; password: string }) {
-    const { email, password } = loginDto;
-    return await this.usersService.loginUser(email, password);
-  }
-
-  @Post('refresh')
-  async refreshToken(@Req() request: Request) {
-    const token = request.headers['authorization']?.split(' ')[1];
-    if (!token) {
-      throw new HttpException('Refresh token not provided', HttpStatus.BAD_REQUEST);
-    }
-    return await this.usersService.refreshToken(token);
   }
 
   @Get()
