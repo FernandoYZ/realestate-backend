2387be1f78969a5a2894f217adb57c9aebc8c039
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 17:11:19 -0500

    refactor(auth): simplificar módulo de autenticación
    Se ha eliminado la inyección del JwtService en los proveedores del AuthModule, ya que no es necesario para el funcionamiento del AuthService. El módulo sigue importando ConfigModule, UsersModule y PassportModule, y mantiene la configuración asíncrona de JwtModule para la gestión de secretos y opciones de firma. Esta modificación mejora la claridad del código y reduce la complejidad innecesaria en la inyección de dependencias.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
@@ -14,16 +14,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async(configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
           expiresIn: configService.get<string>('JWT_EXPIRATION_TIME'),
-        }
-      })
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtService],
+  providers: [AuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
