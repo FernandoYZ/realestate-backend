1c5a6a52fc049d4a1b8a5d2a4aa7c6c90c527deb
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 07:46:31 -0500

    refactor(users): eliminar dependencias de JWT del UsersModule
    Se ha actualizado el UsersModule para eliminar la importación del JwtModule, enfocándose únicamente en la gestión de usuarios. Esto mejora la separación de responsabilidades, delegando la lógica de autenticación y manejo de tokens al AuthModule. Además, se ha añadido la exportación de UsersService para permitir su uso en otros módulos.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,18 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from './entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    JwtModule.register({
-      global: true,
-      secret: 'jwt_secret',
-      signOptions: { expiresIn: '6h' }, 
-    }),
   ],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
