8d95d0c364d21c71185052ff04b7971c7e1ed38e
FernandoYZ <fernandozavala266@gmail.com>
2024-08-29 19:32:07 -0500

    ci[app]: actualizó con el módulo products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductsModule],
 })
 export class AppModule {}
