3511952131849da4b28486acec54f581f962acf8
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 06:39:12 -0500

    refactor(controller): actualizar UsersController para manejar registro
    Se ha modificado el UsersController para incluir nuevos endpoints para el registro de usuarios, inicio de sesión y refresco de tokens. Ahora se manejan las solicitudes de autenticación y se implementan validaciones para asegurar que los tokens se proporcionen correctamente. También se ha mejorado la estructura del controlador para manejar operaciones CRUD de manera asíncrona.
    
    Closes #0

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, HttpException, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Post('register')
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: { email: string; password: string }) {
+    const { email, password } = loginDto;
+    return await this.usersService.loginUser(email, password);
+  }
+
+  @Post('refresh')
+  async refreshToken(@Req() request: Request) {
+    const token = request.headers['authorization']?.split(' ')[1];
+    if (!token) {
+      throw new HttpException('Refresh token not provided', HttpStatus.BAD_REQUEST);
+    }
+    return await this.usersService.refreshToken(token);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll() {
+    return await this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.usersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.usersService.remove(id);
   }
 }
