1c4263ae645e8d21e5c9e2386319635feb6d6a85
FernandoYZ <fernandozavala266@gmail.com>
2024-08-29 22:26:56 -0500

    feat(products): generar resource de products sin archivos de prueba
    Se ha generado el resource de products utilizando el comando 'nest g res products --no-spec'. Esto incluye la creación del módulo, controlador, servicio, DTO y entidades, optimizando la estructura del proyecto al no incluir archivos de prueba.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/realstate'),
-    ProductsModule
+    ProductsModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
