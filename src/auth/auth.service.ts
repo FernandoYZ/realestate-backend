1ffd1bfd9b1431b7864e90c7ad15a7984cd0ea09
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 14:30:31 -0500

    feat(auth): mejorar en servicios de autenticación
    Se ha actualizado el servicio AuthService para incluir un control de intentos de inicio de sesión. Ahora se limita el número de intentos fallidos a 5 en un período de 10 minutos, lanzando una excepción AppManyRequest si se superan. Además, se han implementado excepciones personalizadas AppException para manejar errores de autenticación y se ha mejorado la generación de tokens utilizando ConfigService para obtener secretos y tiempos de expiración. Esto refuerza la seguridad y la gestión de errores en el proceso de autenticación.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,51 +1,99 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import { AppException, AppManyRequest } from 'src/app.exception';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly usersService: UsersService,
-        private readonly jwtService: JwtService
-      ) {}
-    
-      async validateUser(email: string, password: string): Promise<any> {
-        const user = await this.usersService.findByEmail(email);
-        if (user && (await bcrypt.compare(password, user.password))) {
-          return {
-            email: user.email,
-            sub: user._id,
-            name: user.name,
-          };
-        }
-        return null;
+  private readonly maxLoginAttempts = 5;
+  private readonly loginAttemptWindow = 10 * 60 * 1000;
+  private loginAttempts = new Map<string, { attempts: number; firstAttemptTime: number }>();
+
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return {
+        email: user.email,
+        sub: user._id,
+        name: user.name,
+      };
+    }
+    return null;
+  }
+
+  async login(email: string, password: string) {
+    this.checkLoginAttempts(email);
+
+    const user = await this.validateUser(email, password);
+    if (!user) {
+      this.incrementLoginAttempts(email);
+      throw new AppException('Credenciales no válidas');
+    }
+
+    this.resetLoginAttempts(email); 
+    const tokens = await this.generateTokens(user);
+    return { ...tokens, user };
+  }
+
+  async refreshToken(refreshToken: string) {
+    try {
+      const user = this.jwtService.verify(refreshToken, {
+        secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      });
+      return await this.generateTokens(user);
+    } catch (error) {
+      const message = error.name === 'TokenExpiredError'
+        ? 'El token de actualización ha caducado'
+        : 'Token de actualización inválido';
+        throw new AppException(message);
+    }
+  }
+
+  private async generateTokens(user): Promise<{ access_token: string; refresh_token: string }> {
+    const payload = { sub: user.sub, email: user.email, name: user.name };
+    const accessToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_EXPIRATION_TIME'),
+    });
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRATION_TIME'),
+    });
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
+  }
+
+  private checkLoginAttempts(email: string) {
+    const attemptInfo = this.loginAttempts.get(email);
+    const currentTime = Date.now();
+
+    if (attemptInfo) {
+      if (currentTime - attemptInfo.firstAttemptTime > this.loginAttemptWindow) {
+        this.resetLoginAttempts(email);
+      } else if (attemptInfo.attempts >= this.maxLoginAttempts) {
+        throw new AppManyRequest('Demasiados intentos de inicio de sesión, inténtelo de nuevo más tarde');
       }
-      
-      
-    
-      async login(user: any) {
-        const payload = { email: user.email, sub: user._id, name: user.name };
-        return {
-          access_token: this.jwtService.sign(payload),
-          refresh_token: await this.generateRefreshToken(payload),
-        };
-      }
-    
-      async generateRefreshToken(payload: any) {
-        return this.jwtService.sign(payload, { secret: 'jwt_secret_refresh', expiresIn: '1d' });
-      }
-    
-      async refreshToken(refreshToken: string) {
-        try {
-          const user = this.jwtService.verify(refreshToken, { secret: 'jwt_secret_refresh' });
-          const payload = { sub: user._id, email: user.email, name: user.name };
-          return {
-            access_token: this.jwtService.sign(payload),
-            refresh_token: await this.generateRefreshToken(payload),
-          };
-        } catch {
-          throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
-        }
-      }      
+    }
+  }
+
+  private incrementLoginAttempts(email: string) {
+    const attemptInfo = this.loginAttempts.get(email) || { attempts: 0, firstAttemptTime: Date.now() };
+    attemptInfo.attempts += 1;
+    this.loginAttempts.set(email, attemptInfo);
+  }
+
+  private resetLoginAttempts(email: string) {
+    this.loginAttempts.delete(email);
+  }
 }
