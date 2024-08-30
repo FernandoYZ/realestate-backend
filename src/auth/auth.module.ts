d1f07dd1f42f35011eee1d37ff934e3d0687ad9c
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 14:33:37 -0500

    feat(auth): refactorizar módulo de autenticación
    Se ha actualizado el AuthModule para utilizar JwtModule.registerAsync, permitiendo la inyección de ConfigService para obtener el secreto y los tiempos de expiración de los tokens desde la configuración. Esto mejora la flexibilidad y la seguridad del módulo al evitar la exposición de secretos en el código. Además, se ha importado ConfigModule para asegurar que las variables de entorno estén disponibles en el contexto del módulo.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,19 +3,27 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'JWT_SECRET',
-      signOptions: { expiresIn: '6h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async(configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION_TIME'),
+        }
+      })
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
   exports: [AuthService],
 })
 export class AuthModule {}
