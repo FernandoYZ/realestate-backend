e8e131afa1036c40c2fca4b93d30f3aefaad1159
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 06:37:51 -0500

    refactor(users): actualizar UsersService para manejar autenticación y operaciones CRUD
    Se ha modificado el UsersService para incluir la lógica de creación de usuarios, inicio de sesión y gestión de tokens JWT. Ahora utiliza Mongoose para interactuar con la base de datos, incluyendo validaciones para evitar la creación de usuarios duplicados y el manejo de errores para operaciones CRUD. También se ha implementado el hashing de contraseñas con bcrypt.
    
    Closes #0

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,117 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+type Tokens = {
+  access_token: string;
+  refresh_token: string;
+};
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtSvc: JwtService
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.userModel.findOne({ email: createUserDto.email });
+    if (existingUser) {
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return await newUser.save();
+  }
+
+  async loginUser(email: string, password: string) {
+    const user = await this.userModel.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new HttpException('Por favor revisa tus credenciales', HttpStatus.UNAUTHORIZED);
+    }
+
+    const payload = { sub: user._id, email: user.email, name: user.name };
+    const { access_token, refresh_token } = await this.generateTokens(payload);
+
+    return {
+      access_token,
+      refresh_token,
+      user,
+      message: 'Login Successful',
+    };
+  }
+
+  async refreshToken(refreshToken: string) {
+    try {
+      const user = this.jwtSvc.verify(refreshToken, {
+        secret: 'jwt_secret_refresh',
+      });
+      const payload = { sub: user._id, email: user.email, name: user.name };
+      return await this.generateTokens(payload);
+    } catch (error) {
+      console.error('Error verifying refresh token:', error);
+      if (error.name === 'TokenExpiredError') {
+        throw new HttpException('Refresh token has expired', HttpStatus.UNAUTHORIZED);
+      }
+      throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
+    }
+  }
+  
+
+  private async generateTokens(user): Promise<Tokens> {
+    const jwtPayload = { sub: user._id, email: user.email, name: user.name };
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtSvc.signAsync(jwtPayload, {
+        secret: 'jwt_secret',
+        expiresIn: '6h',
+      }),
+      this.jwtSvc.signAsync(jwtPayload, {
+        secret: 'jwt_secret_refresh',
+        expiresIn: '1d',
+      }),
+    ]);
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find().exec(); // Devuelve todos los usuarios
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const user = await this.userModel.findById(id).exec();
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+    if (!updatedUser) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const deletedUser = await this.userModel.findByIdAndDelete(id).exec();
+    if (!deletedUser) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return { message: 'User deleted successfully' };
   }
 }
