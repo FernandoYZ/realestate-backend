afc9b35f4235ba05f3d41f49bd9ff5f0e42e4951
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 07:43:39 -0500

    feat(auth): actualizar AuthModule para incluir dependencias de UsersModule, Passport y JWT
    Se ha modificado el AuthModule para importar UsersModule, PassportModule y JwtModule. Esto permite la integración de la lógica de autenticación con la gestión de usuarios y la implementación de estrategias de autenticación basadas en JWT. Además, se ha configurado el módulo JWT con opciones de firma, mejorando la seguridad y la modularidad de la aplicación.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'JWT_SECRET',
+      signOptions: { expiresIn: '6h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
