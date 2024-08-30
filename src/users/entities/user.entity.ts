28285c18a652fc7b314c04440b21953c83068719
FernandoYZ <fernandozavala266@gmail.com>
2024-08-30 06:32:25 -0500

    refactor(user): actualizar entidad User con propiedades y esquema de Mongoose
    Se ha modificado la entidad User para incluir propiedades como name, lastname, identity_document, gender, email y password, utilizando decoradores de Mongoose. Esta actualización permite una mejor integración con la base de datos y asegura que los datos del usuario sean validados correctamente.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    lastname: string;
+
+    @Prop({ required:true })
+    identity_document: number;
+
+    @Prop({ required:false, enum: ['Masculino', 'Femenino', 'Otros'] })
+    gender: string;
+
+    @Prop({ required:true, unique:true })
+    email: string;
+
+    @Prop({ required:true })
+    password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
