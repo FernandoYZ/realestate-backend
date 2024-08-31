e39bd374b5a8efb86dfd7fba140c0e93b26e6e3a
FernandoYZ <fernandozavala266@gmail.com>
2024-08-31 08:12:42 -0500

    refactor(permissions): actualizar módulo de permisos
    Se ha modificado el módulo de permisos para incluir la configuración de Mongoose, permitiendo la interacción con la base de datos para la entidad Permission. Además, se ha agregado la exportación del servicio de permisos para que pueda ser utilizado en otros módulos de la aplicación.

--- src/Security/permissions/permissions.module.ts
+++ src/Security/permissions/permissions.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Permission, PermissionSchema } from './entities/permission.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ 
+      name: Permission.name, 
+      schema: PermissionSchema 
+    }]),
+  ],
   controllers: [PermissionsController],
   providers: [PermissionsService],
-})
-export class PermissionsModule {}
+  exports: [PermissionsService],
+  })
+export class PermissionsModule {}
