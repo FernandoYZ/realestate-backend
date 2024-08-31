19b0aac4aabd18cfa88b314118ab1ed321fbb3ec
FernandoYZ <fernandozavala266@gmail.com>
2024-08-31 07:03:54 -0500

    refactor(auth): actualizar importaciones
    Se ha movido la carpeta 'users' dentro de la carpeta 'security' para mejorar la estructura del proyecto. Se actualiza la importación de UsersService en auth.controller.ts para reflejar la nueva ubicación.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/security/users/users.module';
 
 @Module({
   imports: [
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { AppException, AppManyRequest } from 'src/app.exception';
+import { UsersService } from 'src/security/users/users.service';
 
 @Injectable()
 export class AuthService {
