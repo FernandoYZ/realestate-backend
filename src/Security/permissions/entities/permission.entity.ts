95cbdc0a0566e9c3d2433669d0626af892061c2c
FernandoYZ <fernandozavala266@gmail.com>
2024-08-31 08:15:11 -0500

    refactor(entity): implementar esquema de permisos con Mongoose
    Se ha actualizado la entidad Permission para utilizar Mongoose, definiendo un esquema que incluye propiedades como name, description, module, submodules y status. Se ha añadido un esquema anidado para Submodule y se han implementado validaciones y valores predeterminados. Esto permite una mejor integración con la base de datos y una estructura más robusta para los permisos.

--- src/Security/permissions/entities/permission.entity.ts
+++ src/Security/permissions/entities/permission.entity.ts
@@ -1,1 +1,37 @@
-export class Permissions {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { v4 as uuidv4 } from 'uuid';
+import { Status } from "../dto/create-permission.dto";
+
+export type PermissionDocument = Permission & Document;
+
+@Schema()
+class Submodule {
+    @Prop({ required: true, default: () => uuidv4() })
+    id: string;
+
+    @Prop({ required: true })
+    name: string;
+}
+
+const SubmoduleSchema = SchemaFactory.createForClass(Submodule);
+
+@Schema()
+export class Permission extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop()
+    description: string;
+
+    @Prop({ required: true, unique: true, index: true })
+    module: string;
+
+    @Prop({ type: [SubmoduleSchema], required: true })
+    submodules: Submodule[];
+
+    @Prop({ required: true, enum: Status, default: Status.activated })
+    status: Status;
+}
+
+export const PermissionSchema = SchemaFactory.createForClass(Permission);
