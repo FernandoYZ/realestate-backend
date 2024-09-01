4e99f610afe4ef08eb865f3f4bb279805e5e7531
FernandoYZ <fernandozavala266@gmail.com>
2024-09-01 05:51:34 -0500

    feat(enums): Crear enumeración de estados para el proyecto
    - Se creó la carpeta 'enums' dentro de 'src' de manera manueal para contener arreglos fijos del proyecto.
    - Se añadió el archivo 'status.enum.ts' con la enumeración estados 'Status' que incluye diferentes estados como 'Desactivado', 'Activado', 'Pendiente', entre otros.
    
    No hay issues
    #Close 0

---
+++ src/enums/status.enum.ts
@@ -0,0 +1,11 @@
+export enum Status {
+    disabled = "Desactivado",
+    activated = "Activado",
+    pending = "Pendiente",
+    deleted = "Eliminado",
+    processing = "En proceso",
+    completed = "Completado",
+    failed = "Fallido",
+    reembolsied = "Reembolsado",
+    verified = "Verificado" 
+}
