1e508341e8550be9ec789e9a7a0773ddb2e8b58c
FernandoYZ <fernandozavala266@gmail.com>
2024-09-01 05:43:20 -0500

    refactor(permissions): Cambiar verificación de permisos existentes - Se modificó la lógica de creación de permisos para verificar la existencia de permisos por módulo en lugar de por nombre. - Se añadió el método lean() en la consulta de indOne para mejorar el rendimiento al obtener un objeto simple de JavaScript. - Se mejoró el mensaje de error en el método emove para ser más específico.
    No hay issues
    #Close 0

--- src/Security/permissions/permissions.service.ts
+++ src/Security/permissions/permissions.service.ts
@@ -10,53 +10,52 @@ import { AppConflict, AppNotFound } from 'src/app.exception';
 export class PermissionsService {
   constructor(
     @InjectModel(Permission.name) private permissionModel: Model<PermissionDocument>,
-) {}
+  ) {}
 
-async create(createPermissionDto: CreatePermissionDto): Promise<Permission> {
+  async create(createPermissionDto: CreatePermissionDto): Promise<Permission> {
     const existingPermission = await this.permissionModel.findOne({ 
-        name: createPermissionDto.name 
+        module: createPermissionDto.module 
     });
 
     if (existingPermission) {
-      throw new AppConflict('Permiso');
+        throw new AppConflict('Permiso');
     }
 
     const newPermission = new this.permissionModel(createPermissionDto);
     return await newPermission.save();
-}
+  }
 
-async findAll(): Promise<Permission[]> {
-    return await this.permissionModel.find().exec();
-}
+  async findAll(): Promise<Permission[]> {
+      return await this.permissionModel.find().exec();
+  }
 
-async findOne(id: string): Promise<Permission> {
-    const permission = await this.permissionModel.findById(id).exec();
-    if (!permission) {
-      throw new AppNotFound('Permiso');
-    }
-    return permission;
-}
+  async findOne(id: string): Promise<Permission> {
+      const permission = await this.permissionModel.findById(id).lean().exec();
+      if (!permission) {
+        throw new AppNotFound('Permiso');
+      }
+      return permission;
+  }
 
-async update(id: string, updatePermissionDto: UpdatePermissionDto): Promise<Permission> {
-    const updatedPermission = await this.permissionModel.findByIdAndUpdate(
-        id, 
-        updatePermissionDto, 
-        { new: true },
-    ).exec();
+  async update(id: string, updatePermissionDto: UpdatePermissionDto): Promise<Permission> {
+      const updatedPermission = await this.permissionModel.findByIdAndUpdate(
+          id, 
+          updatePermissionDto, 
+          { new: true },
+      ).exec();
 
-    if (!updatedPermission) {
-      throw new AppNotFound('Permiso');
-    }
+      if (!updatedPermission) {
+        throw new AppNotFound('Permiso');
+      }
 
-    return updatedPermission;
-}
+      return updatedPermission;
+  }
 
-async remove(id: string): Promise<{ message: string }> {
-    const deletedPermission = await this.permissionModel.findByIdAndDelete(id).exec();
-    if (!deletedPermission) {
-      throw new AppNotFound('Permiso');
-    }
-
-    return { message: 'Permission deleted successfully' };
-}
+  async remove(id: string): Promise<{ message: string }> {
+      const deletedPermission = await this.permissionModel.findByIdAndDelete(id).exec();
+      if (!deletedPermission) {
+        throw new AppNotFound('Permiso no encontrado');
+      }
+      return { message: 'Permiso eliminado exitosamente' };
+  }
 }
