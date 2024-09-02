35af60bdb1ca8019150bad17a002b13e52ca0c1b
FernandoYZ <fernandozavala266@gmail.com>
2024-09-01 19:28:42 -0500

    refactor(module): Cambiar importación en permissions.module.ts
    - Se actualizó la importación de 'Permission' y 'PermissionSchema' a './schemas/permission.schema' para mejorar la organización del código.
    - Este cambio se realizó para alinear el uso de esquemas en lugar de entidades, facilitando una estructura más ordenada y clara en el recurso de permisos.
    
    No hay issues
    #Close 0

--- src/Security/permissions/permissions.module.ts
+++ src/Security/permissions/permissions.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Permission, PermissionSchema } from './entities/permission.entity';
+import { Permission, PermissionSchema } from './schemas/permission.schema';
 
 @Module({
   imports: [
