0c5cae484791001967a2c059168a26fb18388ade
FernandoYZ <fernandozavala266@gmail.com>
2024-09-01 19:19:11 -0500

    refactor(main): integrar swagger y validación
    - Se integró ValidationPipe para validar y transformar las solicitudes entrantes, asegurando que los datos sean correctos antes de ser procesados.
    - Se configuró Swagger para la documentación de la API, permitiendo una mejor visualización y prueba de los endpoints.
    - Se estableció un prefijo global 'api' para todas las rutas, mejorando la organización de la API.
    
    No hay issues
    #Close 0

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './app.exception';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  const config = new DocumentBuilder()
+        .setTitle('API de Ejemplo')
+        .setDescription('Documentación de la API de ejemplo')
+        .setVersion('1.0')
+        .addTag('ejemplo')
+        .build();
+    
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
