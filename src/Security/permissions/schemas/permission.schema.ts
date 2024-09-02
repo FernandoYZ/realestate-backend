e8ad29046454dba2bae2fa38c78af7ca552dfe7b
FernandoYZ <fernandozavala266@gmail.com>
2024-09-01 19:25:34 -0500

    feat(permission): Crear permission.schema.ts y separar lógica de negocio
    - Se creó permission.schema.ts para definir el esquema de Mongoose de Permission, incluyendo propiedades como module, description, y status.
    - Se añadieron timestamps automáticos para 'createdAt' y 'updatedAt' en el esquema.
    - Se separó la lógica de negocio de la entidad, manteniendo la entidad PermissionEntity para representar la lógica de negocio y los datos de la aplicación.
    - Se mejoró la organización del código al separar esquemas, entidades y DTOs en el recurso de permisos.
    
    No hay issues
    #Close 0

---
+++ src/Security/permissions/schemas/permission.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { Status } from "../../../enums/status.enum";
+
+export type PermissionDocument = Permission & Document;
+
+@Schema({ timestamps: true })
+export class Permission extends Document {
+    @Prop({ required: true, unique: true, index:true })
+    module: string;
+
+    @Prop({ required:false, default: 'Acceso al módulo' })
+    description: string;
+
+    @Prop({ required: true, enum: Status, default: Status.activated })
+    status: Status;
+}
+
+export const PermissionSchema = SchemaFactory.createForClass(Permission);
+PermissionSchema.index({ module:1 });
