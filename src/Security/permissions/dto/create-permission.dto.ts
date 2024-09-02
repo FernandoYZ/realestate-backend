c0ee1a641b4810605a9949f2f102ffc2e96bf482
FernandoYZ <fernandozavala266@gmail.com>
2024-09-01 19:27:21 -0500

    refactor(dto): Mejorar CreatePermissionDto con validaciones adicionales
    - Se añadieron validaciones de longitud para el campo 'module', asegurando que tenga entre 3 y 50 caracteres.
    - Se implementó una expresión regular para que el campo 'module' solo acepte letras y espacios, evitando números.
    - Se añadió una validación de longitud para el campo 'description', permitiendo un máximo de 250 caracteres.
    - Se cambió la validación del campo 'status' a opcional, permitiendo que no se especifique en la creación de permisos.
    
    No hay issues
    #Close 0

--- src/Security/permissions/dto/create-permission.dto.ts
+++ src/Security/permissions/dto/create-permission.dto.ts
@@ -1,16 +1,19 @@
-import { IsNotEmpty, IsString, IsOptional, IsEnum } from "class-validator";
+import { IsNotEmpty, IsString, IsOptional, IsEnum, Length, Matches } from "class-validator";
 import { Status } from "../../../enums/status.enum";
 
 export class CreatePermissionDto {
     @IsString()
     @IsNotEmpty()
+    @Length(3, 50, { message: 'El módulo debe tener como máximo 50 caracteres.' })
+    @Matches(/^[a-zA-Z\s]+$/, { message: 'El módulo solo puede contener letras y espacios, sin números.' })
     readonly module: string;
     
     @IsString()
     @IsOptional()
+    @Length(0, 250, { message: 'La descripción debe tener como máximo 250 caracteres.' })
     readonly description?: string;
     
     @IsEnum(Status)
-    @IsNotEmpty()
-    readonly status:Status;
+    @IsOptional()
+    readonly status: Status;
 }
