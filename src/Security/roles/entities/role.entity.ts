00f4712b90370bcc2d4f5868bf4f36238a0cfa0b
FernandoYZ <fernandozavala266@gmail.com>
2024-09-05 20:09:59 -0500

    feat(entity): Crear RoleEntity con propiedades y constructor
    - Se ha creado 'role.entity.ts' que define la clase 'RoleEntity'.
    - La clase incluye propiedades como 'name', 'description', 'actions' y 'status', utilizando enumeraciones para asegurar la integridad de los datos.
    - Se ha implementado un constructor para inicializar las propiedades de la entidad de rol.
    
    No hay issues
    #Close 0

--- src/Security/roles/entities/role.entity.ts
+++ src/Security/roles/entities/role.entity.ts
@@ -1,1 +1,16 @@
-export class Role {}
+import { Actions } from "src/enums/actions.enum";
+import { Status } from "src/enums/status.enum";
+
+export class RoleEntity {
+    name: string;
+    description?: string;
+    actions: Actions;
+    status: Status;
+
+    constructor(name:string, actions:Actions,status:Status, description?:string) {
+        this.name = name;
+        this.description = description;
+        this.actions = actions;
+        this.status = status;
+    }
+}
