9e6ba7099ae1a8b5e8fc2a07cca52965887c03c9
FernandoYZ <fernandozavala266@gmail.com>
2024-09-05 20:08:45 -0500

    feat(service): Implementar lógica de roles con Mongoose y excepciones personalizadas
    - Se ha implementado la lógica para crear, encontrar, actualizar y eliminar roles utilizando Mongoose.
    - Se ha añadido la verificación de conflictos al crear un nuevo rol utilizando 'AppConflict'.
    - Se ha mejorado la gestión de errores al buscar y eliminar roles utilizando 'DataNotFound' y 'AppNotFound'.
    - La estructura del servicio ahora permite un manejo más robusto de los roles en la aplicación.
    
    No hay issues
    #Close 0

--- src/Security/roles/roles.service.ts
+++ src/Security/roles/roles.service.ts
@@ -1,26 +1,67 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Role, RoleDocument } from './schemas/role.schema';
+import { Model } from 'mongoose';
+import { AppConflict, AppNotFound, DataNotFound } from 'src/app.exception';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectModel(Role.name) private roleModel: Model<RoleDocument>,
+  ) 
+  {}
+  async create(createRoleDto: CreateRoleDto): Promise<{ role: Role, message: string }> {
+    const existingRole = await this.roleModel.findOne({
+      name: createRoleDto.name
+    });
+
+    if (existingRole) {
+      throw new AppConflict('Role');
+    }
+
+    const newRole = new this.roleModel(createRoleDto);
+    const saveRole = await newRole.save();
+    return {
+      role: saveRole,
+      message: "Nuevo role agregado con éxito!"
+    }
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(): Promise<Role[]> {
+    return await this.roleModel.find();
+    // return await this.roleModel.find().exec(); //Por si se quiere evitar los datos creado por MongoDB
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: string): Promise<Role> {
+    const role = await this.roleModel.findById(id).lean().exec();
+    if(!role) {
+      throw new AppNotFound('Rol')
+    }
+    return role;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: string, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const updatedRole = await this.roleModel.findByIdAndUpdate(
+      id,
+      updateRoleDto,
+      { new: true },
+    ).exec()
+
+    DataNotFound('Rol', updatedRole);
+
+    return updatedRole;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: string): Promise<{ data:Role, message:string}> {
+    const deletedRole = await this.roleModel.findByIdAndDelete(id).exec();
+    DataNotFound('Rol',deletedRole);
+    return {
+      data: deletedRole,
+      message: "El rol ha sido eliminado con éxito!" 
+    };
   }
 }
+
+  
