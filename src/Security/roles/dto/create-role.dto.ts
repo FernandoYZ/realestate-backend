80ccfbf86c2ad70c2297596b2f92c63a768a9211
FernandoYZ <fernandozavala266@gmail.com>
2024-09-05 20:10:36 -0500

    feat(dto): Implementar CreateRoleDto con validaciones
    - Se ha creado 'create-role.dto.ts' que define la clase 'CreateRoleDto' para la creación de roles.
    - Se han añadido validaciones utilizando 'class-validator' para asegurar que los datos de entrada sean correctos:
      - 'name' debe ser una cadena no vacía con una longitud entre 3 y 50 caracteres.
      - 'description' es opcional y puede tener una longitud máxima de 250 caracteres.
      - 'actions' y 'status' son enumeraciones que validan los valores permitidos.
    
    No hay issues
    #Close 0

--- src/Security/roles/dto/create-role.dto.ts
+++ src/Security/roles/dto/create-role.dto.ts
@@ -1,1 +1,23 @@
-export class CreateRoleDto {}
+import { IsEnum, IsNotEmpty, IsOptional, IsString, Length } from "class-validator";
+import { Actions } from "src/enums/actions.enum";
+import { Status } from "src/enums/status.enum";
+
+export class CreateRoleDto {
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 50, { message: "El nombre debe tener como máximo 50 caracteres"})
+    readonly name: string;
+
+    @IsString()
+    @IsOptional()
+    @Length(0, 250)
+    readonly description?: string;
+
+    @IsEnum(Actions)
+    @IsOptional()
+    readonly actions: string;
+
+    @IsEnum(Status)
+    @IsOptional()
+    readonly status: string;
+}
