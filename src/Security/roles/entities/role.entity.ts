173a8483dd1649d5054f26f2a83b6ab905aaaec0
FernandoYZ <fernandozavala266@gmail.com>
2024-09-08 16:07:34 -0500

    fix(role): Actualizar RoleEntity
    - Se ha actualizado el archivo ole.entity.ts para eliminar el campo ctions, ya que no es necesario en la nueva estructura de la entidad Role.
    - La clase RoleEntity ahora solo incluye los campos
    ame, description y status.
    
    No hay issues
    #Close 0

--- src/Security/roles/entities/role.entity.ts
+++ src/Security/roles/entities/role.entity.ts
@@ -1,16 +1,13 @@
-import { Actions } from "src/enums/actions.enum";
 import { Status } from "src/enums/status.enum";
 
 export class RoleEntity {
     name: string;
     description?: string;
-    actions: Actions;
     status: Status;
 
-    constructor(name:string, actions:Actions,status:Status, description?:string) {
+    constructor(name:string, status:Status, description?:string) {
         this.name = name;
         this.description = description;
-        this.actions = actions;
         this.status = status;
     }
 }
