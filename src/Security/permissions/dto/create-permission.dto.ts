da9a778b0016b2fa71a86fc70feea436bab228f3
FernandoYZ <fernandozavala266@gmail.com>
2024-09-07 22:39:13 -0500

    refactor(permission): Mejorar validaciones en CreatePermissionDto
    - Se añadieron mensajes personalizados a las validaciones del módulo y submódulo para mayor claridad.
    - Se incluyó un nuevo campo submodule con validaciones similares al módulo.
    - Se mejoró la expresión regular para permitir caracteres acentuados en el módulo.
    
    No hay issues
    #Close 0

--- src/Security/permissions/dto/create-permission.dto.ts
+++ src/Security/permissions/dto/create-permission.dto.ts
@@ -2,11 +2,16 @@ import { IsNotEmpty, IsString, IsOptional, IsEnum, Length, Matches } from "class
 import { Status } from "../../../enums/status.enum";
 
 export class CreatePermissionDto {
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: 'El módulo debe contener solo letras' })
+    @IsNotEmpty({ message: 'El módulo no debe estar vacío' })
     @Length(3, 50, { message: 'El módulo debe tener como máximo 50 caracteres.' })
-    @Matches(/^[a-zA-Z\s]+$/, { message: 'El módulo solo puede contener letras y espacios, sin números.' })
+    @Matches(/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/, { message: 'El módulo solo puede contener letras y espacios, sin números.' })
     readonly module: string;
+
+    @IsString({ message: 'El submódulo debe contener solo letras' })
+    @IsNotEmpty({ message: 'El submódulo no debe estar vacío' })
+    @Length(3,50, { message: 'El submódulo debe contener como máximo 50 caracteres.' })
+    readonly submodule: string;
     
     @IsString()
     @IsOptional()
