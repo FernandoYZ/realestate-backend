6bc51b5c61e4ef8d9254b133811cccd1c612eafc
FernandoYZ <fernandozavala266@gmail.com>
2024-09-08 00:31:17 -0500

    feat(cors): Habilitar CORS en la aplicación
    - Se añadió la configuración de CORS en el archivo main.ts para permitir solicitudes desde http://localhost:3000.
    - Se especificaron los métodos permitidos: GET, HEAD, PUT, PATCH, POST, DELETE, y se habilitaron las credenciales.
    
    No hay issues
    #Close 0

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,11 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials:true
+  });
   await app.listen(4000);
 }
 bootstrap();
