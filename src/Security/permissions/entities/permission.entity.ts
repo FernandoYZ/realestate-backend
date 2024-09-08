92a106d3336ab26a40e98f32edf11914c070021f
FernandoYZ <fernandozavala266@gmail.com>
2024-09-07 22:39:38 -0500

    refactor(permission): Añadir submódulo a PermissionEntity
    - Se añadió el campo submodule a la clase PermissionEntity para permitir una mejor organización de permisos.
    - Se actualizó el constructor para incluir el nuevo parámetro submodule.
    
    No hay issues
    #Close 0

--- src/Security/permissions/entities/permission.entity.ts
+++ src/Security/permissions/entities/permission.entity.ts
@@ -2,11 +2,13 @@ import { Status } from "../../../enums/status.enum";
 
 export class PermissionEntity {
     module: string;
+    submodule: string;
     description?: string;
     status: Status;
 
-    constructor(module: string, status: Status, description?: string) {
+    constructor(module: string, submodule:string, status: Status, description?: string) {
         this.module = module;
+        this.submodule = submodule,
         this.description = description;
         this.status = status;
     }
