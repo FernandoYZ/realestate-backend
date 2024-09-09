fc36cc4bf7d71e985525417211d9d5d965663e30
FernandoYZ <fernandozavala266@gmail.com>
2024-09-09 05:32:37 -0500

    refactor(user): renombrar archivo de entity a schema
    - Se cambió el nombre del archivo de 'user.entity.ts' a 'user.schema.ts'.
    - Se mantuvo la estructura y funcionalidad del esquema, solo se actualizó el nombre del archivo.
    
    No hay issues
    #Close 0

---
+++ src/Security/users/schemas/user.schema.ts
@@ -0,0 +1,50 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema } from "mongoose";
+import mongooseAutoPopulate from "mongoose-autopopulate";
+import { Gender } from "src/enums/gender.enum";
+import { Status } from "src/enums/status.enum";
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    lastname: string;
+
+    @Prop({
+        required: true,
+        type: [{
+            type: MongooseSchema.Types.ObjectId,
+            ref: 'Access',
+            autopopulate: {
+                select: 'role permissions',
+                populate: [
+                    { path: 'role', select: 'name' },
+                    { path: 'permissions', select: 'module' }
+                ]
+            }
+        }]
+    })
+    access: MongooseSchema.Types.ObjectId[];
+
+    @Prop({ required:true, unique: true, maxlength:8, minlength:8 })
+    identity_document: number;
+
+    @Prop({ required:true, enum: Gender, default: [] })
+    gender: string;
+
+    @Prop({ required:true, unique:true })
+    email: string;
+
+    @Prop({ required:true, minlength: 8 })
+    password: string;
+
+    @Prop({ required: true, enum: Status, default: Status.activated})
+    status: Status;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.plugin(require('mongoose-autopopulate'));
