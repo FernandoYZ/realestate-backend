7f63969fa2c4a7e038ac29a5dd74af5232d94ebf
FernandoYZ <fernandozavala266@gmail.com>
2024-09-08 19:56:54 -0500

    refactor(actions): Reestructurar Actions y agregar clases relacionadas
    - Se ha modificado el enum Actions para cambiar los nombres de los valores a un formato más descriptivo (ej. isCreate, isRead, etc.).
    - Se ha añadido la clase ActionsBoolean para representar las acciones como booleanos, facilitando la gestión de permisos.
    - Se ha introducido la clase ActionsDto para validar un array de acciones, utilizando decoradores de class-validator para asegurar que las acciones sean únicas y válidas.
    
    No hay issues
    #Close 0

--- src/enums/actions.enum.ts
+++ src/enums/actions.enum.ts
@@ -1,6 +1,36 @@
+import { IsOptional } from "class-validator";
+
 export enum Actions {
-    create = "Agregar",
-    read = "Ver",
-    update = "Actualizar",
-    delete = "Eliminar"
+    isCreate = "Agregar",
+    isRead = "Ver",
+    isUpdate = "Actualizar",
+    isDelete = "Eliminar" 
+}
+
+export class ActionsBoolean {
+    isView: boolean;    
+    isEdit: boolean;    
+    isDelete: boolean;  
+    isCreate: boolean;  
+
+    constructor(isView: boolean, isEdit: boolean, isDelete: boolean, isCreate: boolean) {
+        this.isView = isView;    
+        this.isEdit = isEdit;    
+        this.isDelete = isDelete;  
+        this.isCreate = isCreate;  
+    }
+}
+
+export class ActionsDto {
+    @IsOptional()
+    readonly isView?: boolean;    
+
+    @IsOptional()
+    readonly isEdit?: boolean;    
+
+    @IsOptional()
+    readonly isDelete?: boolean;  
+
+    @IsOptional()
+    readonly isCreate?: boolean;  
 }
