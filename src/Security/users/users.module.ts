ef30658d22699140a83043976c1b5f5e0f2f4384
FernandoYZ <fernandozavala266@gmail.com>
2024-09-09 05:34:26 -0500

    refactor(users.module.ts): actualizar importaciones tras renombrar archivo
    - Se cambió la importación del esquema de usuario de ./entities/user.entity a ./schemas/user.schema.
    - Se mantuvo la estructura y funcionalidad del módulo de usuarios.
    
    No hay issues
    #Close 0

--- src/Security/users/users.module.ts
+++ src/Security/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema, User } from './entities/user.entity';
+import { UserSchema, User } from './schemas/user.schema';
 
 @Module({
   imports: [
