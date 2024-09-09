b210819b46d48601157584c550b44014d6ed89f3
FernandoYZ <fernandozavala266@gmail.com>
2024-09-08 19:58:57 -0500

    feat(access): Implementar esquema de Access con autopoblación
    - Se ha creado el esquema Access utilizando decoradores de NestJS y Mongoose.
    - Se ha añadido la propiedad ole como referencia a Role, con autopoblación habilitada.
    - Se ha añadido la propiedad permissions, que es un array de referencias a Permission, también con autopoblación.
    - Se ha incluido un array de ctions que utiliza el enum Actions.
    - Se ha añadido la propiedad status, que utiliza el enum Status y tiene un valor por defecto de Status.pending.
    - Se ha habilitado el plugin mongoose-autopopulate para facilitar la carga automática de las referencias.
    
    No hay issues
    #Close 0

--- src/Security/access/schemas/access.schema.ts
+++ src/Security/access/schemas/access.schema.ts
@@ -1,1 +1,45 @@
-export class Access {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema } from "mongoose";
+import mongooseAutoPopulate from "mongoose-autopopulate";
+import { Actions } from "src/enums/actions.enum";
+import { Status } from "src/enums/status.enum";
+
+export type AccessDocument = Access & Document;
+
+@Schema({ timestamps: true })
+export class Access extends Document {
+    @Prop({
+        required: true,
+        type: MongooseSchema.Types.ObjectId,
+        ref: 'Role',
+        autopopulate: true
+    })
+    role: MongooseSchema.Types.ObjectId;
+
+    @Prop({
+        required: true,
+        type: [{
+            type: MongooseSchema.Types.ObjectId,
+            ref: 'Permission',
+            autopopulate: true
+        }]
+    })
+    permissions: MongooseSchema.Types.ObjectId[];
+
+    @Prop({
+        type: [String],
+        enum: Actions,
+        default: []
+    })
+    actions: Actions[];
+
+    @Prop({
+        required: true,
+        enum: Status,
+        default: Status.pending
+    })
+    status: Status;
+}
+
+export const AccessSchema = SchemaFactory.createForClass(Access);
+AccessSchema.plugin(require('mongoose-autopopulate'));
