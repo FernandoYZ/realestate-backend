e4134b21e0e4073616d73831e38a1833032ae6f0
FernandoYZ <fernandozavala266@gmail.com>
2024-09-09 05:33:43 -0500

    refactor(CreateUserDto): actualizar validaciones y agregar campos
    - Se añadieron mensajes personalizados a las validaciones de los campos
    ame, lastname, identity_document, mail, password y gender.
    - Se agregó el campo ccess como un array de MongooseSchema.Types.ObjectId con validaciones para asegurar que contenga al menos un elemento.
    - Se incluyó el campo status como opcional con validación de enumeración.
    - Se mantuvo la estructura general del DTO.
    
    No hay issues
    #Close 0

--- src/Security/users/dto/create-user.dto.ts
+++ src/Security/users/dto/create-user.dto.ts
@@ -1,34 +1,40 @@
-import { IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, Length, MinLength } from "class-validator";
-
-export enum Gender {
-    Masculino = 'Masculino',
-    Femenino = 'Femenino',
-    Otros = 'Otros',
-}
+import { ArrayNotEmpty, IsArray, IsEmail, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Length, MinLength } from "class-validator";
+import { Schema as MongooseSchema } from "mongoose";
+import { Gender } from "src/enums/gender.enum";
+import { Status } from "src/enums/status.enum";
 
 export class CreateUserDto {
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: 'Por favor, ingrese un nombre válido (solo letras).' })
+    @IsNotEmpty({ message: 'El nombre es obligatorio. ¡Por favor, complete este campo!' })
     readonly name: string;
     
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: 'Por favor, ingrese un apellido válido (solo letras).' })
+    @IsNotEmpty({ message: 'El apellido es obligatorio. ¡Por favor, complete este campo!' })
     readonly lastname: string;
     
-    @IsInt()
-    @IsNotEmpty()
-    @Length(8,8)
+    @IsString({ message: 'El documento de identidad debe ser un número entero.' })
+    @IsNotEmpty({ message: 'El documento de identidad es necesario. ¡Por favor, complete este campo!' })
+    @Length(8, 8, { message: 'El documento de identidad debe tener exactamente 8 dígitos.' })
     readonly identity_document: number;
+
+    @IsArray()
+    @ArrayNotEmpty({ message: 'El acceso debe contener al menos un elemento.' })
+    @IsNotEmpty({ each: true, message: 'Cada elemento de acceso debe ser un ObjectId válido.' })
+    readonly access: MongooseSchema.Types.ObjectId[];
     
     @IsOptional()
-    @IsEnum(Gender)
+    @IsEnum(Gender, { message: 'Por favor, seleccione un género válido.' })
     readonly gender?: Gender;
 
-    @IsNotEmpty()
-    @IsEmail()
+    @IsNotEmpty({ message: 'El correo electrónico es necesario. ¡Por favor, complete este campo!' })
+    @IsEmail({}, { message: 'Por favor, ingrese un correo electrónico válido.' })
     readonly email: string;
 
-    @IsNotEmpty()
-    @MinLength(8)
+    @IsNotEmpty({ message: 'La contraseña es obligatoria. ¡Por favor, complete este campo!' })
+    @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
     readonly password: string;
+
+    @IsEnum(Status, { message: 'Por favor, seleccione un estado válido.' })
+    @IsOptional()
+    readonly status?: Status;
 }
